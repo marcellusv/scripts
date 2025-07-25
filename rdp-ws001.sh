8b0c24e751a2a46c2b2a67537a8f3a55b7c756c2
Marcel V. <marcellusv@users.noreply.github.com>
2025-07-25 14:51:01 +0200

    Add xfreerdp script for remote work

---
+++ rdp-ws001.sh
@@ -0,0 +1,15 @@
+xfreerdp3 \
+	/gateway:g:remote.gateway.com \
+	/v:ws001.domain.com \
+	/sound:sys:pulse \
+	/microphone:sys:pulse \
+	/audio-mode:auto \
+	/size:1920x1080 \
+	/compression \
+	/compression-level:2 \
+	/bpp:8 \
+	-themes \
+	-fonts \
+
+
+
