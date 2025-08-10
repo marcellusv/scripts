f70e8f20dffa9f43e431f660194d8a279c2b57e7
Marcel V. <marcellusv@users.noreply.github.com>
2025-08-10 12:20:53 +0200

    Cleanup the volume percentage script

--- statusbar/vol.sh
+++ statusbar/vol.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/sh
 sink=$(pactl list short | grep RUNNING | awk '{print $1}')
-vol=$(pactl get-sink-volume $sink | awk '{print $5}')
-echo "$vol"
+if [[ ! -z $sink ]]; then
+	vol=$(pactl get-sink-volume $sink | awk '{print $5}')
+	echo "$vol"
+else
+	echo "-"
+fi
