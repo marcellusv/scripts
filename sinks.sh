2f5b2fea0ff9a564aab886b29ced84062c03bd23
Marcel V. <marcellusv@users.noreply.github.com>
2025-08-31 08:15:59 +0200

    Get the active sink

--- sinks.sh
+++ sinks.sh
@@ -4,7 +4,7 @@ add_sink() {
 	if [[ $batt == "" ]]; then
 		batt="-"
 	fi
-
+	
 	sink_ids+=("$id")
 	sink_names+=("$name")
 	sink_descriptions+=("$desc")
@@ -31,6 +31,8 @@ sink_volumes=()
 sink_batteries=()
 
 
+sink_active=$(pacmd list-sinks | awk '/\*/ {getline; print $2}' | sed 's/[<>]//g')
+
 sink_info=$(pactl list sinks | grep -e "Sink #" -e "Name:" -e "Description:" -e "^[[:space:]]Volume:" -e "bluetooth.battery")
 
 while IFS= read -r line; do
@@ -41,7 +43,7 @@ while IFS= read -r line; do
 			add_sink
 		fi
 
-		id=("$(echo "$line" | awk -F' #' '{print $1}' )")
+		id=("$(echo "$line" | awk -F' #' '{print $2}' )")
 	elif [[ $line == *"Name"* ]]; then
 		name=("$(echo "$line" | awk -F': ' '{print $2}' )")
 	elif [[ $line == *"Description"* ]]; then
@@ -64,3 +66,5 @@ add_sink
 for i in "${!sink_names[@]}"; do
 	echo "${sink_ids[$i]}) ${sink_descriptions[$i]} ${sink_names[$i]} ${sink_volumes[$i]} ${sink_batteries[$i]}"
 done
+
+echo $sink_active
