80d873d011ca38726dd7c21ac0f2dff59293fe0b
Marcel V. <marcellusv@users.noreply.github.com>
2025-10-05 22:57:09 +0200

    Show the calendar when clicking

--- statusbar/time.sh
+++ statusbar/time.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/sh
+case $BUTTON in
+	1) notify-send "" "$(cal --monday --week)" ;;
+	3) st -e sh -c "cal --three --monday --week; read" ;;
+esac
+
 dwmdate=$(date '+%a %d-%b %H:%M %V')
 printf "$dwmdate"
