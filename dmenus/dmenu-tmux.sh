83e20df564be0bc0049d6366dbf01b0171be6cc3
Marcel V. <marcellusv@users.noreply.github.com>
2025-10-07 11:42:36 +0200

    Re-use redundant code via a function

--- dmenus/dmenu-tmux.sh
+++ dmenus/dmenu-tmux.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Fetch a list of all the available tmux sessions
-sessions=$(tmux list-sessions -F "#S" 2>/dev/null)
+function open_session {
+	local session_name="$1"
 
-# Fetch tmux clients tty, select the first active only
-active_tty=$(tmux list-clients -F "#{client_tty}" | head -n 1)
+	# Fetch tty of all tmux clients, select the first active only
+	local active_tty=$(tmux list-clients -F "#{client_tty}" | head -n 1)
+
+	# Try to re-use an attached tmux client in the active terminal (st)
+	if [[ -n "$active_tty" ]]; then
+		tmux switch-client -c "$active_tty" -t "$session_name"
+	else
+		st -e tmux attach-session -t "$session_name" &
+	fi
+}
+
+# Fetch a list of all available tmux sessions
+sessions=$(tmux list-sessions -F "#S" 2>/dev/null)
 
 # Prompt the user to select a session
 selected=$(echo -e "[New Session]\n$sessions" | dmenu -i -c -l 30 -p "tmux sessions:")
@@ -14,18 +25,10 @@ if [[ "$selected" == "[New Session]" ]]; then
 	new_name=$(echo '' | dmenu -c -p "Enter new session name:")
 	if [[ -n "$new_name" ]]; then
 		tmux new-session -ds "$new_name"
-		if [[ -n "$active_tty" ]]; then
-			tmux switch-client -c "$active_tty" -t "$new_name"
-		else
-			st -e tmux attach-session -t "$new_name" &
-		fi
+		open_session "$new_name"
 	fi
 
 # Option 2: User selects an existing session
 elif [[ -n "$selected" ]]; then
-	if [[ -n "$active_tty" ]]; then
-		tmux switch-client -c "$active_tty" -t "$selected"
-	else
-		st -e tmux attach-session -t "$selected" &
-	fi
+	open_session "$selected"
 fi
